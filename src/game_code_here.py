b80d13b39dcaed83b3f7303e6afa811fea17827f
wfelcone <wfelcone@ucvts.org>
2020-01-07 09:57:27 -0500

    changed window color

--- src/game_code_here.py
+++ src/game_code_here.py
@@ -1,6 +1,24 @@
 import pygame
+import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode( (500, 500) )
+screen = pygame.display.set_mode( (1000, 400) )
+
+classic_blue = (15, 76, 129)
+
+myfont=pygame.font.SysFont('Comic Sans MS', 12)
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+
+    pygame.time.delay(100)
+    screen.fill(classic_blue)
+    pygame.display.flip()
+
+    textsurface = myfont.render("We Live in a Soceity"), False, (0, 0, 0))
+    win.blit(textsurface, (10, 10))
+    pygame.display.flip()
 
